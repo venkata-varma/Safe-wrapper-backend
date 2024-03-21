5dc6af51f7e5ebdf040cfa3cc54cdeaf0fabd364
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-03-21 10:57:56 +0530

    Latest Pull Updates

--- controllers/workOrderControllers.js
+++ controllers/workOrderControllers.js
@@ -474,7 +474,7 @@ exports.pullLatestWorkOrders = asyncWrapper(async (req, res) => {
                                 /* Search by'Created', 'AcknowledgeBy', 'OnSiteBy', 'DueDate', 'LastUpdate'*/
                                 "Created": {
                                     "From": "2024-02-06T00:00:00Z",
-                                    "To": new Date()
+                                    "To": "2024-02-14T24:00:00.000Z"
                                     // "To": "2024-02-07T24:00:00.000Z"
                                 }
                                 /*Search by work order status -> New,Accepted,Recalled,Rejected,CheckedIn,Paused,CheckedOut,OnHold,Verified,NeedsCompletionDetails*/
