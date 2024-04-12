758543e167269c490bd04e65408d64146ef07393
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-04-12 13:03:38 +0530

    CronJob handle changes update Apr-12

--- handler/workOrderAndInvoiceDetailsUpdate.js
+++ handler/workOrderAndInvoiceDetailsUpdate.js
@@ -1,13 +1,8 @@
-const { workOrdersIntegrations, invoicesIntegrations } = require('../cronJobs/cronjobs')
+const {workOrdersIntegrations,invoicesIntegrations} = require('../cronJobs/cronjobs')
 
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = async(event, context,callback)=>{
 
-    let date = new Date()
-    let hours = date.getHours().toString()
-    let minutes = date.getMinutes().toString()
-    if (hours === "09" && minutes === "00") {
-        workOrdersIntegrations()
-        invoicesIntegrations()
-    }
+    workOrdersIntegrations()
+    invoicesIntegrations()
     callback(null)
 };
