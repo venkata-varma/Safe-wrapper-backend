3778d5b783aef5eaced80be2c1d1137c3521790d
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-05-27 10:47:48 +0530

    Api changes updated may 27

--- controllers/workOrdersController.js
+++ controllers/workOrdersController.js
@@ -1,9 +1,6 @@
 const asyncWrapper = require("../middleware/asyncWrapper");
 const integrationsMasterServiceProvidersModel = require("../models/integrationsMasterServiceProvidersModel");
 
-
-const fieldMappingsMasterModel = require("../models/fieldMappingsMasterModel");
-
 exports.get_corrigo_pro_latest_workOrders = asyncWrapper(async (req, res) => {
     const { integrationServiceProviderId } = req.params;
     const configguration_credentials = await integrationsMasterServiceProvidersModel.findById(integrationServiceProviderId).lean();
