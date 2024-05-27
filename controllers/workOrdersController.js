6bfa3f9279227a5a2a0cf5f2de1f8757fe4be272
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-05-27 12:18:15 +0530

    API changes updated and new branch created

--- controllers/workOrdersController.js
+++ controllers/workOrdersController.js
@@ -5,3 +5,4 @@ exports.get_corrigo_pro_latest_workOrders = asyncWrapper(async (req, res) => {
     const { integrationServiceProviderId } = req.params;
     const configguration_credentials = await integrationsMasterServiceProvidersModel.findById(integrationServiceProviderId).lean();
 });
+
