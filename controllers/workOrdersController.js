8a5ea30361986b4554cafcb07ae2accc410ddb95
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-05-27 12:20:54 +0530

    integration api changes updated

--- controllers/workOrdersController.js
+++ controllers/workOrdersController.js
@@ -5,4 +5,3 @@ exports.get_corrigo_pro_latest_workOrders = asyncWrapper(async (req, res) => {
     const { integrationServiceProviderId } = req.params;
     const configguration_credentials = await integrationsMasterServiceProvidersModel.findById(integrationServiceProviderId).lean();
 });
-
