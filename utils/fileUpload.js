9ed022f28b272918a2294ff37bf21f9c41691e86
venkatvarma92 <venkatavarma.peethala@devrabbit.com>
2024-06-26 15:03:16 +0530

    New folder

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -7,19 +7,19 @@ const path=require('path')
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, 'uploads/'); // 'uploads/' is the folder where files will be stored
+            cb(null, 'accountLogos/'); // 'uploads/' is the folder where files will be stored
         },
         filename: function (req, file, cb) {
-            cb(null, Date.now() + path.extname(file.originalname).replace(/\\/g, "/"))
+            cb(null,file.originalname.replace(/\\/g, "/"))
         }
     }),
-    limits: {
-        fileSize: 2000000
-    },
+    // limits: {
+    //     fileSize: 2000000
+    // },
     fileFilter: (req, file, cb) => {
       let ext = path.extname(file.originalname);
-      if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-        cb(new Error("Only jpg, jpeg, png"), false);
+      if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png"&& ext !== ".webp"&& ext !== ".bmp"&& ext !== ".svg"&& ext !== ".tiff"&& ext !== ".heif"&& ext !== ".raw" ) {
+        cb(new Error("Only Image file types are allowed."), false);
         return;
       }
       cb(null, true);
@@ -37,54 +37,9 @@ const upload = multer({
  * Get the static images.
  */
 
-const getImageUrl =async (fileName) => {
-    console.log('filename', fileName)
-    // Constructing the URL based on the request object
-    const baseUrl = req.protocol + '://' + req.get('host');
-    // const imageUrls = [
-    //   {
-    //     name: "TT",
-    //     url: baseUrl + '/static/TurboTax_logo.png'
-    //   },
-    //   {
-    //     name: "AM",
-    //     url: baseUrl + '/static/Acumatica_logo.png'
-    //   },
-    //   {
-    //     name: "CPD",
-    //     url: baseUrl + '/static/CorrigoPro_logo.png'
-    //   },
-    //   {
-    //     name: "DF",
-    //     url: baseUrl + '/static/Dataforma_logo.png'
-    //   },
-    //   {
-    //     name: "QB",
-    //     url: baseUrl + '/static/Quickbooks_logo.png'
-    //   },
-    //   {
-    //     name: "SC",
-    //     url: baseUrl + '/static/ServiceChannel_logo.png'
-    //   },
-    //   {
-    //     name: "SNOW",
-    //     url: baseUrl + '/static/servicenow_logo.png'
-    //   },
-    //   {
-    //     name: "MDS",
-    //     url: baseUrl + '/static/MDS_logo2.png'
-    //   }
-    // ]
-  let imageUrl=baseUrl + `/static/${fileName}`
-    if (!baseUrl || !imageUrl) {
-      return res.status(500).json({ error: "Unable to construct image URL" });
-    }
-  return imageUrl;
-    
-  }
-  
+
 
   module.exports={
     upload,
-    getImageUrl
+
   }
