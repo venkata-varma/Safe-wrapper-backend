77c82b98476381991b7da88030880d963186e2fe
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-11-15 12:16:08 +0530

    field-mapping-master-tables-name-changes-v1

---
+++ models/webHooksModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+
+const webHooksSchema = new mongoose.Schema({
+    webHookId:{
+        type:mongoose.Schema.Types.ObjectId,
+        default:null
+    },
+    accountId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "accounts",
+        index : true,
+        default: null,
+    },
+    integrationsMasterId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "integrationsMaster",
+        index : true,
+        default : null
+    },
+    webHookUrl: {
+        type:String,
+        default:""
+    },
+    authenticationCode:{
+        type:String,
+        default:""
+    },
+    requestObject:{
+        type: mongoose.Schema.Types.Mixed,
+        default:{}
+    },
+    status:{
+        type:String,
+        enum:['active','delete'],
+        default:"active"
+    }
+},{timestamps:true})
+
+webHooksSchema.pre('save',function(next){
+    this.webHookId = this._id,
+    next()
+})
+
+module.exports = mongoose.model('webhooksmodel',webHooksSchema)
