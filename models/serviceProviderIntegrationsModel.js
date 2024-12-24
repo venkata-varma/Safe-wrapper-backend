d3d0bdff165293521994e0aaff019ff96c7ea487
ChanduDevRabbit <chandusai.pendyala@devrabbit.com>
2024-12-24 17:05:44 +0530

    get-service-order-dynamic-v1

--- models/serviceProviderIntegrationsModel.js
+++ models/serviceProviderIntegrationsModel.js
@@ -1,76 +1,74 @@
 const mongoose = require('mongoose')
 
 const serviceProviderIntegrations = new mongoose.Schema({
-    serviceProviderIntegrationId:{
-        type:mongoose.Schema.Types.ObjectId,
-        index:true,
-        default:null
+    serviceProviderIntegrationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        index: true,
+        default: null
     },
-    fromServiceProviderListId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'serviceProviderList',
-        index:true,
-        default:null
+    fromServiceProviderListId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'serviceProviderList',
+        index: true,
+        default: null
     },
-    toServiceProviderListId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'serviceProviderList',
-        index:true,
-        default:null
+    toServiceProviderListId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'serviceProviderList',
+        index: true,
+        default: null
     },
-    name:{
-        type:String,
-        default:""
+    name: {
+        type: String,
+        default: ""
     },
-    from :{
-        type : String,
-        default : ""
+    from: {
+        type: String,
+        default: ""
     },
-    to : {
-        type : String,
-        default : ""
+    to: {
+        type: String,
+        default: ""
     },
-    services : {
-        type : Array,
-        default : []
+    services: {
+        type: Array,
+        default: []
     },
-    filterServices:{
-        type : Array,
-        default : []
+    filterServices: {
+        type: Array,
+        default: []
     },
-    mapping : {
-        type : Object,
-        default : {}
+    mapping: {
+        type: Object,
+        default: {}
     },
-    settings:[
-        {
-            sourceSettings:{
-                type:Object,
-                default:{}
-            },
-            destinationSettings:{
-                type:Object,
-                default:{}
-            },
-            mappingSettings:{
-                type:Object,
-                default:{}
-            },
-            statusSettings:{
-                type:Object,
-                default:{}
-            }
+    settings: {
+        sourceSettings: {
+            type: Object,
+            default: {}
+        },
+        destinationSettings: {
+            type: Object,
+            default: {}
+        },
+        mappingSettings: {
+            type: Object,
+            default: {}
+        },
+        statusSettings: {
+            type: Object,
+            default: {}
         }
-    ],
-    status:{
-        type:String,
-        default:"active"
+    },
+    status: {
+        type: String,
+        default: "active"
     }
-},{timestamps:true});
+}, { timestamps: true });
 
-serviceProviderIntegrations.pre('save',function(next){
+serviceProviderIntegrations.pre('save', function (next) {
     this.serviceProviderIntegrationId = this._id
     next()
 })
 
-module.exports = mongoose.model('serviceproviderintegrations',serviceProviderIntegrations);
+module.exports = mongoose.model('serviceproviderintegrations', serviceProviderIntegrations);
