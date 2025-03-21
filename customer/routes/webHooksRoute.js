9dd3e1a82cce56849364d19e5dd66fdf8f8e279c
venkatavarma9 <venkatavarma.peethala@devrabbit.com>
2025-03-21 17:36:23 +0530

    need to pull

--- customer/routes/webHooksRoute.js
+++ customer/routes/webHooksRoute.js
@@ -1,42 +1,55 @@
-const router = require('express').Router()
-
+const router = require("express").Router();
 
 const {
-    createWebHook,
-    validateAccountStatusToCreateWebHook,
-    //validateIntegrationForWebHook,
-    //updateWebHook,
-    //getAllWebHooks,
-    //getIndividualWebHook,
-    //updateWebHookStatus,
-    generateWebhookToken,
-    validateWebHookData,
-    createWebHookLog,
-    //getWebHookLogsReports,
-    validateWebHookReceiveData,
-    receiveWebhookData,
+  createWebHook,
+  validateAccountStatusToCreateWebHook,
+  //validateIntegrationForWebHook,
+  //updateWebHook,
+  //getAllWebHooks,
+  //getIndividualWebHook,
+  //updateWebHookStatus,
+  generateWebhookToken,
+  validateWebHookData,
+  createWebHookLog,
+  //getWebHookLogsReports,
+  validateWebHookReceiveData,
+  receiveWebhookData,
+  decryptString,
+  getIndividualWebhookDetails,
+  validateWebhookStatus,
+  validateAccountStatus,
+  getAllWebhooksOfAccount
+} = require("../controllers/webHooksController");
 
-} = require('../controllers/webHooksController')
+router.post(
+  "/:randomNumber/:accountId",
+  validateWebHookReceiveData,
+  receiveWebhookData
+);
 
+const auth = require("../../middleware/authentication");
 
+router.use(auth);
+router.post(
+  "/create-webhook",
+  validateAccountStatusToCreateWebHook,
+  createWebHook
+);
 
-router.post('/:randomNumber/:accountId', validateWebHookReceiveData,receiveWebhookData)
+router.post("/decrypt-string",  decryptString);
+router.get(
+  "/get-individual-webhook-details/:webhookId",
+  validateWebhookStatus,
+  getIndividualWebhookDetails
+);
 
-
-const auth = require('../../middleware/authentication')
-
-
-router.use(auth)
-router.post('/create-webhook',validateAccountStatusToCreateWebHook ,createWebHook)
-
+router.get('/get-all-webhook-details/:accountId', validateAccountStatus,getAllWebhooksOfAccount)
 // router.patch('/update-webhook',validateIntegrationForWebHook, updateWebHook)
 // router.patch('/update-webhook-status',validateIntegrationForWebHook, updateWebHookStatus)
 
-// router.get('/get-all-webhooks',validateIntegrationForWebHook, getAllWebHooks)
+ //router.get('/get-all-webhooks',validateIntegrationForWebHook, getAllWebHooks)
 // router.get('/get-single-webhook',validateIntegrationForWebHook,getIndividualWebHook)
- 
+
 // router.get('/get-webhook-logs-reports',validateIntegrationForWebHook, getWebHookLogsReports)
 
-
-
- module.exports = router
+module.exports = router;
