7c92add6068b604723ced607a2372ae5be58f667
venkatavarma9 <venkatavarma.peethala@devrabbit.com>
2025-10-06 17:30:30 +0530

    single integrration view totally complated - draft 1

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/api-docs', swaggerexpressui.serve, swaggerexpressui.setup(swaggerDocum
 // End of swagger configuration
 
 
-//webhookCrons.webhookScheduleCronJobs()
+webhookCrons.webhookScheduleCronJobs()
 
 //integrationCrons.cardConnectScheduleCronJobs()
 
