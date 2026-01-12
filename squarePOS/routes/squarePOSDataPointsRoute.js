15cf2e20155137198bc327cd8a72ff61bfacfc69
venkatavarma9 <venkatavarma.peethala@devrabbit.com>
2026-01-12 10:26:59 +0530

    middlewres added for two endpoints

--- squarePOS/routes/squarePOSDataPointsRoute.js
+++ squarePOS/routes/squarePOSDataPointsRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 let squarePOSDataPointsController = require('../controllers/squarePOSDataPointsController')
+const { validateAccountExistAndActive } = require('../controllers/squarePOSMasterContollers')
 const auth = require('../../middleware/authentication')
 
 router.use(auth)
 
-router.get('/get-square-pos-payload-headers/:accountId', squarePOSDataPointsController.getSquarePosPayloadHeaders)
-router.post('/get-shifts-transactions-reconcilation/:accountId', squarePOSDataPointsController.getSquarePOSShiftTransactionsReconcilation)
+router.get('/get-square-pos-payload-headers/:accountId', validateAccountExistAndActive, squarePOSDataPointsController.getSquarePosPayloadHeaders)
+router.post('/get-shifts-transactions-reconcilation/:accountId', validateAccountExistAndActive, squarePOSDataPointsController.getSquarePOSShiftTransactionsReconcilation)
 module.exports = router;
